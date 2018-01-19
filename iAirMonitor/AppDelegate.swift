53a13d229a5e4933b937247dfdb67619f2a3f525
arzzcom <arzz@arzz.com>
2018-01-19 10:47:39 +0900

    Initial Commit

---
+++ iAirMonitor/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  iAirMonitor
+//
+//  Created by 알쯔 on 2018. 1. 19..
+//  Copyright © 2018년 Moimz. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
